7934469a337f2a046b9c2a8080d0eda280335602
srsebbi <csperichon@gmail.com>
2022-06-13 21:55:36 -0300

    v 0.2 hover feature

--- app.js
+++ app.js
@@ -2,5 +2,9 @@ const container = document.querySelector('.container')
 for (let i = 0; i <= 255; i++ ) {
     const div = document.createElement('div')
     div.classList.toggle('squareDiv')
+    div.addEventListener('mouseover', e => {
+        e.target.classList.add('hov')
+    })
     container.appendChild(div);
 }
+
