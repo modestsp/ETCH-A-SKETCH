7bb150ffa2ed8353fa05b0955219b3ce69012de2
srsebbi <csperichon@gmail.com>
2022-06-14 14:54:53 -0300

    v 0.4 generate grid button added

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
 const container = document.querySelector('.container')
+const button = document.querySelector('button')
 let size = 16
-for (let i = 1; i <= (size * size); i++ ) {
-    const div = document.createElement('div')
-    div.classList.toggle('squareDiv')
-    div.addEventListener('mouseover', e => {
-        e.target.classList.add('hov')
-    })
-    container.appendChild(div);
+
+generate();
+
+
+function generate() {
+    for(let i = 1; i<= (size * size); i++) {
+        const squareDiv = document.createElement('div')
+        squareDiv.classList.toggle('squareDiv')
+        container.appendChild(squareDiv)
+        squareDiv.addEventListener('mouseover', e => {
+            e.target.classList.add('hov')
+        })
+    } 
 }
 
+button.addEventListener('click', () => {
+    size = parseInt(prompt('Enter new size'))
+    container.style.gridTemplateColumns = `repeat(${size},1fr)`
+    container.style.gridTemplateRows = `repeat(${size}, 1fr)`
+    const squareDiv = document.querySelectorAll('.squareDiv')
+    squareDiv.forEach(div => {
+        div.classList.remove('hov')
+    })
+   generate();
+})
+
+container.style.gridTemplateColumns = `repeat(${size},1fr)`
+container.style.gridTemplateRows = `repeat(${size}, 1fr)`
